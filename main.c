8d9149ba9bf2ab013a44f2ad9db8b34e9eb4c2e6
Chenguang Wang <w3cing@gmail.com>
2023-11-24 10:54:34 +0800

    parse argv: add -h (help), -d (dump)

--- main.c
+++ main.c
@@ -1,8 +1,12 @@
 #include <assert.h>
+#include <string.h>
 
 #include "all.h"
 
+static int dump_debug_info = 0;
+
 static void dump_all(const char *prompt, ParseResult ir) {
+    if (!dump_debug_info) return;
     if (prompt) {
         printf("\n####################\n");
         printf("### %s\n", prompt);
@@ -27,18 +31,25 @@ static void x64(FuncDef *fd) {
     AsmFunc *af;
     af = isel_simple_x64(fd);
 
-    printf("\n####################\n");
-    printf("### %s after isel_simple_x64()\n",
-           Ident_to_str(fd->ident));
-    printf("####################\n\n");
+    if (dump_debug_info) {
+        printf("\n####################\n");
+        printf("### %s after isel_simple_x64()\n", Ident_to_str(fd->ident));
+        printf("####################\n\n");
+    }
     dump_x64(af);
 
     /* TODO: reg alloc */
 }
 
+static void dump_usage(void) {
+    printf("usage: wqbe [OPTIONS] [INPUT]\n");
+    printf("    -d    dump debug info\n");
+    printf("    -h    prints this messsage\n");
+}
+
 int main(int argc, char *argv[]) {
-    FILE *f;
-    int need_close = 0;
+    int i;
+    FILE *f = 0;
     ParseResult ir;
 
     assert(sizeof(Type) == 4);
@@ -51,20 +62,31 @@ int main(int argc, char *argv[]) {
     assert(sizeof(AsmInstr) == 24);
     assert(sizeof(((AsmFunc *) 0)->label[0]) == 8);
 
-    if (argc != 2) {
-        fprintf(stderr, "usage: wqbe INPUT\n");
+    for (i = 1; i < argc; ++i) {
+        if (strcmp(argv[i], "--help") == 0 || strcmp(argv[i], "-h") == 0) {
+            dump_usage();
+            return 0;
+        } else if (strcmp(argv[i], "-d") == 0) {
+            dump_debug_info = 1;
+        } else {
+            check(f == 0, "wqbe: multiple input specified");
+            f = stdin;
+            if (strcmp(argv[i], "-") != 0)
+                f = fopen(argv[i], "r");
+            check(f != 0, "wqbe: failed to open %s", argv[i]);
+        }
+    }
+
+    if (f == 0) {
+        fprintf(stderr,
+                "wqbe: no input specified "
+                "(use '-' for stdin input)\n\n");
+        dump_usage();
         return 1;
     }
 
-    if (argv[1][0] == '-' && argv[1][1] == '\0') {
-        f = stdin;
-    } else {
-        f = fopen(argv[1], "r");
-        check(f != 0, "failed to open %s", argv[1]);
-        need_close = 1;
-    }
     ir = parse(f);
-    if (need_close) fclose(f);
+    if (f != stdin) fclose(f);
     dump_all("after parse()", ir);
 
     run_all_fd(ir.first_funcdef_id, dephi);
